90dca6531a697e1fa243092c569ba1459bd8cd58
cole <cerk8n>
2022-03-08 18:44:49 -0600

    control.py replaces motor.py

--- control.py
+++ control.py
@@ -1,61 +1,86 @@
 #!/usr/bin/python
 
+from tokenize import String
+from numpy import character
 import RPi.GPIO as GPIO
-from lib.Adafruit_PWM_Servo_Driver import PWM
-from lib import xbox_read
+import xbox_read
 import time
 
-# Initialise the PWM device using the default address
-pwm = PWM(0x40, debug=True)
+#pwm.setPWMFreq(60)
 
-# Initialise the GPIO output channels
 GPIO.setmode(GPIO.BCM)
-FORWARD = 21
-BACKWARD = 17
-GPIO.setup(FORWARD, GPIO.OUT)
-GPIO.setup(BACKWARD, GPIO.OUT)
-direction = None
+class Motor:
+    def __init__(self,p1,p2,enable):
+        self.pin1=p1
+        self.pin2=p2
+        GPIO.setup(self.pin1,GPIO.OUT)
+        GPIO.setup(self.pin2,GPIO.OUT)
+        GPIO.setup(self.en,GPIO.OUT)
+        self.pwm=GPIO.PWM(enable,1000)
 
-# Setup direction of travel
-def setDirection(new_direction):
-    global direction # One global property, cut me some slack
-    if direction:
-        GPIO.output(direction,False)
-    GPIO.output(new_direction,True)
-    direction = new_direction
 
-setDirection(FORWARD)
+    def setDir(self, forward):  #1=true=forward, 0=false=backwards
+        if(forward):
+            GPIO.output(self.pin1,1)
+            GPIO.output(self.pin2,0)
+        else:
+            GPIO.output(self.pin1,0)
+            GPIO.output(self.pin2,1)
 
-# Default calibration values 
-servoMid = 425
-servoWidth = 180
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+    def stop(self):
+        GPIO.output(self.pin1, 0)
+        GPIO.output(self.pin2, 0)
 
-rt_intensity = 0
+
+    def setDuty(self, level: character):  #l=low=75%, m=medium=50%, h=high=40%
+        if level=='l':
+            self.pwm.ChangeDutyCycle(75)
+        if level=='m':
+            self.pwm.ChangeDutyCycle(50)
+        if level=='h':
+            self.pwm.ChangeDutyCycle(40)
+
+ 
+def analogsticks(motor: Motor, value : int):
+    motor.setDir(value>0)
+    temp = abs(value)
+    if temp > 32000:
+        motor.setDuty('h')
+    elif temp > 16000:
+        motor.setDuty('m')
+    elif temp > 0:
+        motor.setDuty('l')
+    else:
+        motor.stop()
+
+#Right trigger is upwards, left trigger is downwards
+def triggers(motor: Motor):
+    motor.setDir(rt_intensity>=lt_intensity)
+    temp = max(rt_intensity,lt_intensity)
+    if temp > 250:
+        motor.setDuty('h')
+    elif temp > 0:
+        motor.setDuty('l')
+    else:
+        motor.stop
+
+#We need to assign these pins
+vertmotor = Motor(1,2,3)
+leftmotor = Motor(1,2,3)
+rightmotor = Motor(1,2,3)
 lt_intensity = 0
-steer = servoMid
-
+rt_intensity = 0
 for event in xbox_read.event_stream(deadzone=12000):
-    # Triggers control speed
     if event.key=='RT' or event.key=='LT':
         if event.key=='RT':
             rt_intensity = event.value
         else:
             lt_intensity = event.value
-        # Change direction outputs when one trigger is pulled harder than the other
-        new_direction = FORWARD if rt_intensity>=lt_intensity else BACKWARD
-        if not direction==new_direction:
-            print 'set direction: %s' % {FORWARD:'FORWARD',BACKWARD:'BACKWARD'}[new_direction]
-            setDirection(new_direction)
-        intensity = max(rt_intensity,lt_intensity) * 16
-        pwm.setPWM(0, 0, intensity)
-        print 'set speed: %d' % intensity
-    # Left thumbstick controls the steering
-    if event.key=='X1':
-        steer = int( servoMid + (servoWidth*-event.value)/32768 )
-        pwm.setPWM(1, 0, steer)
-    if event.key=='Y' and event.is_press():
-        servoMid = steer
-        print 'Calibrate steering around %d' % steer
-        
+        triggers(vertmotor) 
+    if event.key=='Y1':
+        analogsticks(leftmotor, event.value)
+    if event.key=='Y2':
+        analogsticks(rightmotor, event.value)
+
+
